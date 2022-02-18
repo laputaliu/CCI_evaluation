fa8df05fcf2d00fb80a48a4bd434bd76a56414d7
laputaliu <49710605+laputaliu@users.noreply.github.com>
2022-02-18 11:21:20 +0800

    Update run_cytotalk.R

--- scripts/run_cytotalk.R
+++ scripts/run_cytotalk.R
@@ -1,45 +1,5 @@
 
-
-################ ----------------------------------------- #################
-
-## you need to split your count matrix by cell types first
-## we finish this step in python, the code of this step can be found in our markdown note of CytoTalk.
-
-## also, don't forget to copy deter_W4.R, gen_PCSF.py, LigandReceptor_Human.txt, ProteinCodingGenes_Human.txt these 
-##   four files into your input dir.
-
-## then, for running Cytotalk, we only need to source this script in R. e.g. source('run_Cytotalk.R')
-
-############################################################################
-
-
-
-
-
-### The objective of this R script: infer a signaling network between two specified cell types using a step-by-step running fasion.
-# System requirements: >= 4 logical cores, >= 8GB memory, >= 10GB disk.
-# Timing: ~6h (2.3 GHz 8-Core Intel Core i9, 16 logical cores and 16GB memory)
-
-
-# There are 10 input parameters for running the CytoTalk algorithm as below.
-#' @param species indicating the species from which the scRNA-Seq data are generated. Currently, “Human” and “Mouse” are supported.
-#' @param CellTypeA specifying the name of cell type A. Please make sure that the cell type name should be consistent with scRNA-seq data file.
-#' @param CellTypeB specifying the name of cell type B. Please make sure that the cell type name should be consistent with scRNA-seq data file.
-#' @param GeneFilterCutoff_A indicating the cutoff for removing lowly-expressed genes in the processing of scRNA-seq data of cell type A. The default cutoff value is 0.1, which means that genes expressed in less than 10% of all cells of cell type A are removed.
-#' @param GeneFilterCutoff_B indicating the cutoff for removing lowly-expressed genes in the processing of scRNA-seq data of cell type B. The default cutoff value is 0.1, which means that genes expressed in less than 10% of all cells of cell type B are removed.
-#' @param BetaUpperLimit indicating the upper limit of the test values of the PCSF objective function parameter β, which is inversely proportional to the total number of genes in a given cell-type pair after removing lowly-expressed genes. Based on preliminary tests, the upper limit of β value is suggested to be 100 (default) if the total number of genes in a given cell-type pair is above 10,000. However, if the total number of genes is below 5000, it is necessary to increase the upper limit of β value to 500.
-#' @param Order indicating the order of the neighborhood of the ligand and receptor to extract their-associated pathway from the predicted signaling network between the two cell types.
-#' @param InputPath specifying the directory that includes necessary input files for running CytoTalk.
-#' @param OutputPath specifying the directory that includes the folder "/IllustratePCSF/" and "/IllustratePathway/" to show the whole predicted network and each ligand-receptor-associated pathway, respectively.
-#' @param WorkingPath specifying the directory that includes all CytoTalk functions.
-
-
-
-####-------- running CytoTalk -----------####
-
-### Customize your inputs and parameters:
 rm(list = ls())
-
 
 library(optparse)
 
@@ -53,6 +13,7 @@ option_list <- list(
   make_option(c("-o", "--outputDir"), type="character",
               help="output dir")
 )
+
 opt_parser <- OptionParser(option_list=option_list,add_help_option = FALSE);
 opts <- parse_args(opt_parser);
 
@@ -67,9 +28,6 @@ GeneFilterCutoff_B <- 0.1
 BetaUpperLimit <- 100
 Order <- 5
 no_cores <- 6
-
-# InputPath <- "/fs/home/liuzhaoyang/project/cci_evaluation/human_heart/tools/CytoTalk/input"
-# OutputPath <- "/fs/home/liuzhaoyang/project/cci_evaluation/human_heart/tools/CytoTalk/output/"
 
 WorkingPath <- "/fs/home/liuzhaoyang/biosoft/CytoTalk_package_v3.1.0/CytoTalk_Function"
 setwd(WorkingPath)
@@ -91,7 +49,6 @@ source("gen_signalingPathway.R")
 
 run_cytotalk <- function(InputPath,OutputPath,WorkingPath,CellTypeA,CellTypeB,species,GeneFilterCutoff_A,GeneFilterCutoff_B,BetaUpperLimit,Order,no_cores){
   print(paste0('>>>>>>>>>  ',CellTypeA, '|', CellTypeB,' <<<<<<<<<<'))
-  print(paste0('>>> load library and data <<< [', Sys.time(),']'))
 
   print(paste0('>>> start CytoTalk workflow <<< [', Sys.time(),']'))
   
@@ -99,18 +56,12 @@ run_cytotalk <- function(InputPath,OutputPath,WorkingPath,CellTypeA,CellTypeB,sp
     dir.create(OutputPath)
   }
   
-  if (substr(OutputPath, nchar(OutputPath), nchar(OutputPath)) != '/'){
-    OutputPath = paste0(OutputPath,'/')
-  }
-  
-  OutputPath_ct = paste0(OutputPath,CellTypeA,'_',CellTypeB)
+  OutputPath_ct = file.path(OutputPath,paste0(CellTypeA,'_',CellTypeB))
   if (!file.exists(OutputPath_ct)){
     dir.create(OutputPath_ct)
   }
 
   #1) Pre-process the scRNA-seq data. ~5min
-  # print("Start running CytoTalk!")
-  # print(Sys.time())
   preprocess(species, CellTypeA, CellTypeB, GeneFilterCutoff_A, GeneFilterCutoff_B, InputPath, OutputPath_ct)
   
   result <- tryCatch({
