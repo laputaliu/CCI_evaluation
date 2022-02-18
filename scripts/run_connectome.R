d7ce7d58282df48f1e8d3e4e47bc1da39e705d80
laputaliu <49710605+laputaliu@users.noreply.github.com>
2022-02-18 11:27:26 +0800

    Update run_connectome.R

--- scripts/run_connectome.R
+++ scripts/run_connectome.R
@@ -1,6 +1,4 @@
 
-
-
 library(optparse)
 library(Connectome)
 library(ggplot2)
@@ -9,12 +7,11 @@ library(Seurat)
 
 
 options(stringsAsFactors = FALSE)
-
 option_list <- list(  
   make_option(c("-c", "--count"), type="character", 
               help="count matrix / normalized count matrix path"),
   make_option(c("-m", "--meta"), type="character",
-              help="meta data (celltypes annotation) path"),
+              help="meta data (celltype annotation) path"),
   make_option(c("-o", "--output"), type="character",
               help="the output dir")
   )
@@ -33,74 +30,39 @@ if (!file.exists(output_path)){
 run_connectome <- function(count_path, meta_path, output_path){
   
   print('############ ------------- connectome --------------- ############')
-  print(paste0('>>> load library and data <<< [', Sys.time(),']'))
-  
-  ## local path
+  print(paste0('>>> loading data <<< [', Sys.time(),']'))
   count_df = read.csv(count_path, sep='\t', row.names=1)
   meta_df = read.table(meta_path,sep='\t')
   
-  if (substr(output_path, nchar(output_path), nchar(output_path)) != '/'){
-    output_path = paste0(output_path,'/')
-  }
-  
   print(paste0('>>> generate seurat object <<< [', Sys.time(),']'))
-  
   seuratObj <- CreateSeuratObject(counts = count_df)
   seuratObj@meta.data$celltype = meta_df$V2
   Idents(object = seuratObj) = meta_df$V2
-  
   ct_list = levels(factor(meta_df$V2))
 
   print(paste0('>>> start Connectome workflow (preprocessing) <<< [', Sys.time(),']'))
-  
-  ##### scale
+  ## scale
   panc8 <- seuratObj
   connectome.genes <- union(Connectome::ncomms8866_human$Ligand.ApprovedSymbol,Connectome::ncomms8866_human$Receptor.ApprovedSymbol)
   genes <- connectome.genes[connectome.genes %in% rownames(panc8)]
   panc8 <- ScaleData(panc8,features = genes)
-  ## min.cells.per.ident set to 5 , the cell(spot) number of Endothelial is just 8
   
   print(paste0('>>> create Connectome object <<< [', Sys.time(),']'))
-  
   panc8.con <- CreateConnectome(panc8,species = 'human',)
   
-  print(paste0('>>> filter Connectome interactions <<< [', Sys.time(),']'))
-  
   ## interaction (edge) filtering
+  print(paste0('>>> filter Connectome interactions <<< [', Sys.time(),']'))
   panc8.con2 <- FilterConnectome(panc8.con,min.pct = 0.1,min.z = 0.25,remove.na = T)
-  # min.pct = 0.1, min.z = 0.5  -->  80 edges
-  # min.pct = 0.1,min.z = 0.25  -->  720 edges
-  
-  print(paste0('>>> analysis finished <<< [', Sys.time(),']'))
   
   print(paste0('>>> plot & save results <<< [', Sys.time(),']'))
-  
-  save(panc8.con2,file=paste0(output_path, 'CCI_network_filtered.rda'))
-  
+  save(panc8.con2,file=file.path(output_path, 'CCI_network_filtered.rda'))
   output_df = panc8.con2$weight_sc
   names(output_df) = panc8.con2$edge
-  write.table(output_df, file=paste0(output_path, 'weight_sc.tsv'),sep='\t',quote = F, col.names = F)
-  
+  write.table(output_df, file=file.path(output_path, 'weight_sc.tsv'),sep='\t',quote = F, col.names = F)
   print(paste0('>>> done <<< [', Sys.time(),']'))
   
 }
 
 
 run_connectome(count_path, meta_path, output_path)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
