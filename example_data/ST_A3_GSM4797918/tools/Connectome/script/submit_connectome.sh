7892a5ceda2e4069f6dca362b994f15b52948215
laputaliu <49710605+laputaliu@users.noreply.github.com>
2022-08-24 14:58:46 +0800

    upload example tool result

---
+++ example_data/ST_A3_GSM4797918/tools/Connectome/script/submit_connectome.sh
@@ -0,0 +1,1 @@
+screen -x connectome -p 0 -X stuff "cd /fs/home/liuzhaoyang/project/cci_evaluation/human_intestinal/ST_A3_GSM4797918 && nohup time -v Rscript /fs/home/liuzhaoyang/project/cci_evaluation/scripts/run_connectome.R -c ./data/processed/sc_norm.tsv -m ./data/processed/sc_meta.tsv -o ./tools/Connectome/output/ > ./tools/Connectome/script/connectome_nohup.out 2>&1 &\n"
---
+++ example_data/ST_A3_GSM4797918/tools/base_line/script/submit_base_line.sh
@@ -0,0 +1,1 @@
+cd /fs/home/liuzhaoyang/project/cci_evaluation/human_intestinal/ST_A3_GSM4797918 && nohup python /fs/home/liuzhaoyang/project/cci_evaluation/scripts/run_base_line.py -c ./data/processed/sc_norm.tsv -m ./data/processed/sc_meta.tsv -o ./tools/base_line/output/ -n 10 > ./tools/base_line/script/nohup.out 2>&1 &
